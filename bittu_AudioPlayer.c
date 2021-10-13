58a05407e5452d2b876db0c12edd1d810b5e0818
tripulse <poli.sb@protonmail.com>
2021-10-13 23:55:36 +0530

    minor fixes

--- bittu_AudioPlayer.c
+++ bittu_AudioPlayer.c
@@ -5,7 +5,7 @@ JNIEXPORT void JNICALL Java_bittu_AudioPlayer_init
   (JNIEnv * env, jobject this)
 {
     jfieldID f_ptr = (*env)->GetFieldID(
-        env, (*env)->GetObjectClass(env, env), "ptr", "J");
+        env, (*env)->GetObjectClass(env, this), "ptr", "J");
 
     // make assumption that NULL is zero.
     (*env)->SetLongField(env, this, f_ptr, player_init());
@@ -15,14 +15,14 @@ JNIEXPORT jint JNICALL Java_bittu_AudioPlayer_stream
   (JNIEnv * env, jobject this, jstring url)
 {
     jfieldID f_ptr = (*env)->GetFieldID(
-        env, (*env)->GetObjectClass(env, env), "ptr", "J");
+        env, (*env)->GetObjectClass(env, this), "ptr", "J");
 
     player_t* ptr = (*env)->GetLongField(env, this, f_ptr);
-    const char* url = (*env)->GetStringUTFChars(env, url, NULL);
+    const char* url_ = (*env)->GetStringUTFChars(env, url, NULL);
 
-    int ret = player_stream(ptr, url);
+    int ret = player_stream(ptr, url_);
 
-    ReleaseStringUTFChars(url);
+    (*env)->ReleaseStringUTFChars(env, url, url_);
     return -ret;
 }
 
@@ -30,13 +30,13 @@ JNIEXPORT jint JNICALL Java_bittu_AudioPlayer_get20ms
   (JNIEnv * env, jobject this, jbyteArray buf)
 {
     jfieldID f_ptr = (*env)->GetFieldID(
-        env, (*env)->GetObjectClass(env, env), "ptr", "J");
+        env, (*env)->GetObjectClass(env, this), "ptr", "J");
 
     player_t* ptr = (*env)->GetLongField(env, this, f_ptr);
     jbyte* out = (*env)->GetByteArrayElements(env, buf, NULL);
 
     size_t out_size; // PLAYER_AUDIO_BUFFER_MAX_SIZE size is mandatory
-    int ret = player_get_20ms_audio(ptr, out, out_size);
+    int ret = player_get_20ms_audio(ptr, out, &out_size);
 
     (*env)->ReleaseByteArrayElements(env, buf, out, 0);
     return ret == PLAYER_NO_ERROR ? out_size : -ret;
@@ -46,7 +46,7 @@ JNIEXPORT void JNICALL Java_bittu_AudioPlayer_uninit
   (JNIEnv * env, jobject this)
 {
     jfieldID f_ptr = (*env)->GetFieldID(
-        env, (*env)->GetObjectClass(env, env), "ptr", "J");
+        env, (*env)->GetObjectClass(env, this), "ptr", "J");
     
     player_uninit((*env)->GetLongField(env, this, f_ptr));
 }
